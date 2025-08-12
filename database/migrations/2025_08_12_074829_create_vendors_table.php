3c22366ee5b42dc2bbb258fc631a9d7c77ec1c0c
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-12 13:18:57 +0530

    fix: update vendors table migration file

--- database/migrations/2025_08_12_074829_create_vendors_table.php
+++ database/migrations/2025_08_12_074829_create_vendors_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('vendors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('contact_person')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
