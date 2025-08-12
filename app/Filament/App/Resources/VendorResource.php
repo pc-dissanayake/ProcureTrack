93baced9d92f38fc872461f71193673c5a3eaf6c
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-12 13:35:34 +0530

    refactor: update VendorResource in Filament app

--- app/Filament/App/Resources/VendorResource.php
+++ app/Filament/App/Resources/VendorResource.php
@@ -1,11 +1,3 @@
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListVendors::route('/'),
-            'create' => Pages\CreateVendor::route('/create'),
-            'edit' => Pages\EditVendor::route('/{record}/edit'),
-        ];
-    }
 <?php
 
 namespace App\Filament\App\Resources;
@@ -18,6 +10,15 @@ use Filament\Tables;
 use Filament\Tables\Table;
 
 class VendorResource extends Resource
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVendors::route('/'),
+            'create' => Pages\CreateVendor::route('/create'),
+            'edit' => Pages\EditVendor::route('/{record}/edit'),
+        ];
+    }
 {
     protected static ?string $model = Vendor::class;
 
