89958580e215bb4bee3f8516e95c24803d3dced0
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-12 13:34:10 +0530

    feat: update Vendor model in pp/Models

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vendor extends Model
 {
+    protected $fillable = [
+        'name',
+        'contacts', // JSON array of contact persons with position, phones, emails
+        'address',
+        'city',
+        'country',
+        'notes',
+    ];
+
+    protected $casts = [
+        'contacts' => 'array',
+    ];
+
+    public function procurements()
+    {
+        return $this->hasMany(Procurement::class);
+    }
+}
     //
 }
