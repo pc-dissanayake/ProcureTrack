ae663f3b084235a5f1c72072cf10c5b8a395fbaa
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-12 13:47:53 +0530

    feat: add VendorResource to Filament app resources

---
+++ app/Filament/App/Resources/VendorResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use App\Models\Vendor;
+use App\Filament\Resources\VendorResource\Pages;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class VendorResource extends Resource
+{
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVendors::route('/'),
+            'create' => Pages\CreateVendor::route('/create'),
+            'edit' => Pages\EditVendor::route('/{record}/edit'),
+        ];
+    }
+    protected static ?string $model = Vendor::class;
+
+    
+}
