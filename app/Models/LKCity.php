6f3a3907c238a5c2c875d8890ff248c0c5e427cd
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-12 13:58:37 +0530

    feat: update LKCity model in pp/Models

--- app/Models/LKCity.php
+++ app/Models/LKCity.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class LKCity extends Model
 {
-    //
+    protected $fillable = [
+        'district_id',
+        'name_en',
+        'name_si',
+        'name_ta',
+        'sub_name_en',
+        'sub_name_si',
+        'sub_name_ta',
+        'postcode',
+        'latitude',
+        'longitude',
+    ];
 }
